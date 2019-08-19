6b1154de45367f944bb2c027101368936b290859
Sune Thomsen <47953133+SuneThomsenDK@users.noreply.github.com>
2019-08-19 14:34:21 +0200

    Add files via upload

--- ToastNotification/Show-ToastNotification.ps1
+++ ToastNotification/Show-ToastNotification.ps1
@@ -3,16 +3,16 @@
 	Language detection for toasted notifications in Windows 10.
 
 .DESCRIPTION
-    This script will check for "System Local" language in registry.
-    0414 = nb_NO
-    0406 = da_DK
-    0409 = en_US
+	This script will check for "System Local" language in registry.
+	0414 = nb_NO
+	0406 = da_DK
+	0409 = en_US
 
 .NOTES
-	Version: 1.9.8.15
+	Version: 1.9.8.19
 	Author: Sune Thomsen
 	Creation date: 15-08-2019
-	Last modified date: 15-08-2019
+	Last modified date: 19-08-2019
 
 .LINK
 	https://www.osdsune.com
@@ -20,19 +20,21 @@
 
 $RegKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Nls\Language"
 $RegName = "Default"
-$daDKRegValue = "0406"
-$enUSRegValue = "0409"
-$nbNORegValue = "0414"
+$RegValue_daDK = "0406"
+$RegValue_nbNO = "0414"
 $OSLanguage = Get-ItemPropertyValue -Path $RegKey -Name $RegName -ErrorAction SilentlyContinue
 
-if ($OSLanguage -eq "$daDKRegValue"){
-PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-daDK.xml"
-}
+	if ($OSLanguage -eq "$RegValue_daDK"){
+		PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-daDK.xml"
+		break
+	}
 
-if ($OSLanguage -eq "$enUSRegValue"){
-PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-enUS.xml"
-}
+	if ($OSLanguage -eq "$RegValue_nbNO"){
+		PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-nbNO.xml"
+		break
+	}
 
-if ($OSLanguage -eq "$nbNORegValue"){
-PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-nbNO.xml"
-}
+	if (($OSLanguage -ne "$RegValue_daDK") -and ($OSLanguage -ne "$RegValue_nbNO")){
+		PowerShell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File $PSScriptRoot\New-ToastNotification.ps1 -Config "$PSScriptRoot\Config\config-toast-update-enUS.xml"
+		break
+	}
