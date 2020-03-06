a42e166f9ee44a0491fa0d0f56512bd4ddd6e31e
Sune Thomsen <47953133+SuneThomsenDK@users.noreply.github.com>
2020-03-06 17:22:49 +0100

    Add files via upload

---
+++ Scripts/Reset_Local_Admin_Password.ps1
@@ -0,0 +1,10 @@
+﻿#Create EncodedPass
+#$NewPass = 'New Password Here'
+#$EncodedPass = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($NewPass))
+#$EncodedPass
+
+#Reset Local Admin Password
+$EncodedPass = 'TgBlAHcAUABhAHMAcwB3AG8AcgBkAEgAZQByAGUA'
+$DecodedPass = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($EncodedPass))
+$Username = 'Administrator'
+Net User $Username $DecodedPass
