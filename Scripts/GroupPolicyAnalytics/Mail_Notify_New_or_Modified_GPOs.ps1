157b491815e4dc52ec8769389559b00a6ab98cb4
Sune Thomsen <47953133+SuneThomsenDK@users.noreply.github.com>
2024-07-04 10:22:08 +0200

    GPO Monitoring Report
    GPO Monitoring Report

--- Scripts/GroupPolicyAnalytics/Mail_Notify_New_or_Modified_GPOs.ps1
+++ Scripts/GroupPolicyAnalytics/Mail_Notify_New_or_Modified_GPOs.ps1
@@ -26,39 +26,39 @@ If (!(Test-Path -PathType Container $ReportPath)){
 # Main Script
 ForEach ($BaseOU in $LimitedSubOUs){
 
-    ForEach ($LinkedGroupPolicyObjects in $BaseOU.LinkedGroupPolicyObjects){
+	ForEach ($LinkedGroupPolicyObjects in $BaseOU.LinkedGroupPolicyObjects){
 
-        # Extraxt GUID from policy object
-        $GUID = $LinkedGroupPolicyObjects | Select-String -Pattern '{[-0-9A-F]+?}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
-        
-        # Collect data if not already run for this GPO
-        If ($GUID -notin $AllGPOStats.Guid){
+		# Extraxt GUID from policy object
+		$GUID = $LinkedGroupPolicyObjects | Select-String -Pattern '{[-0-9A-F]+?}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
 
-            $GPO = $null
-            Try {
-                $GPO = Get-GPO -Guid $GUID | Where {((Get-Date)-[datetime]$_.ModificationTime).days -lt $VarDays}
-            }
-            Catch {
-                #Write-Host ("Error getting infromation for the GPO with the GUID: {0}" -f $GUID)
-            }
+		# Collect data if not already run for this GPO
+		If ($GUID -notin $AllGPOStats.Guid){
+
+			$GPO = $null
+			Try {
+				$GPO = Get-GPO -Guid $GUID | Where {((Get-Date)-[datetime]$_.ModificationTime).days -lt $VarDays}
+			}
+			Catch {
+				#Write-Host ("Error getting infromation for the GPO with the GUID: {0}" -f $GUID)
+			}
 
 
-            If ($GPO){
-                #Write-Host ("Adding information for the GPO with the GUID: {0} to the AllGPOStats object" -f $GUID)
-                # Define PSCustomObject
-                $Obj = [PSCustomObject]@{
-            
-                    #Variables
-                    DisplayName = $GPO.DisplayName
-                    GPOStatus = $GPO.GPOStatus
-                    CreationTime = $GPO.CreationTime
-                    ModificationTime = $GPO.ModificationTime
-                    Guid = ("{" + ('{0}') -f ($GPO.Id).Tostring() + "}").ToUpper()
-                }
+			If ($GPO){
+				#Write-Host ("Adding information for the GPO with the GUID: {0} to the AllGPOStats object" -f $GUID)
+				# Define PSCustomObject
+				$Obj = [PSCustomObject]@{
 
-                # Add to report
-                $AllGPOStats += $Obj
-				
+					#Variables
+					DisplayName = $GPO.DisplayName
+					GPOStatus = $GPO.GPOStatus
+					CreationTime = $GPO.CreationTime
+					ModificationTime = $GPO.ModificationTime
+					Guid = ("{" + ('{0}') -f ($GPO.Id).Tostring() + "}").ToUpper()
+				}
+
+				# Add to report
+				$AllGPOStats += $Obj
+
 				# HTML style
 				$HeadStyle = "<style>"
 				$HeadStyle = $HeadStyle + "BODY{background-color:White;}"
@@ -67,11 +67,11 @@ ForEach ($BaseOU in $LimitedSubOUs){
 				$HeadStyle = $HeadStyle + "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:Gainsboro}"
 				$HeadStyle = $HeadStyle + "</style>"
 
-                Write-Output $AllGPOStats
+				Write-Output $AllGPOStats
 				$AllGPOStats | ConvertTo-Html -Head $HeadStyle -Body "<h2>$($ENV:ComputerName) Report - $Date1</h2>" | Out-File $ReportLocation -Force
-            }
-        }
-    }
+			}
+		}
+	}
 }
 
 # Send GPO Monitoring Report if exist.
